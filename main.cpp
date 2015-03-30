88995b5583f866bc070a472583fc55513999dfcf
TitaniaAnn <titaniaann@gmail.com>
2015-03-30 07:38:56 -0700

    Adding main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Primer Plus
+//
+//  Created by Cynthia Brown on 3/30/15.
+//  Copyright (c) 2015 Cynthia Brown. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
